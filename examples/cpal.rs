dd30832b52122c550fb1dbfdc30a8d4519cbd0a3
Ryan B <notryanb@gmail.com>
2017-11-03 14:49:37 -0400

    start messing around trying to get cpal up to date and working. Then try to spawn in background threads

--- examples/cpal.rs
+++ examples/cpal.rs
@@ -28,79 +28,137 @@ fn main() {
 
     // Pick a playback format supported by the endpoint, which matches the spec
     // of the wav file.
-    let format = endpoint.get_supported_formats_list().unwrap()
-                         .filter(|f| matches_format(f, &spec))
-                         .next()
-                         .expect("no supported playback format");
+    let format = endpoint
+        .supported_formats()
+        .unwrap()
+        .next()
+        .expect("no supported playback format")
+        .with_max_samples_rate();
+
+    let samples_rate = format.samples_rate.0 as f32;
+    let mut sample_clock = 0f32;
+    let samples = reader.samples().map(|s| s.unwrap()).collect();
+
+    let mut next_value = || {
+        sample_clock = (sample_clock + 1.0) % samples_rate;
+        (sample_clock * 440.0 / samples_rate)
+    };
+
+
+    // Start Build Voice
+
+    let event_loop = cpal::EventLoop::new();
+    let mut voice_id = event_loop.build_voice(&endpoint, &format).unwrap();
+
+    event_loop.play(voice_id);
+
+
+    // End Build Voice
+    //
+    // Start P.ayback
+    event_loop.run(move |_, buffer| {
+        match buffer {
+            cpal::UnknownTypeBuffer::U16(mut buffer) => {
+                for sample in buffer.chunks_mut(format.channels.len()) {
+                    // let value = ((next_value() * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
+                    for out in sample.iter_mut() {
+                        *out = value;
+                    }
+                }
+            },
+
+            cpal::UnknownTypeBuffer::I16(mut buffer) => {
+                for sample in buffer.chunks_mut(format.channels.len()) {
+                    // let value = (next_value() * std::i16::MAX as f32) as i16;
+                    for out in sample.iter_mut() {
+                        *out = value;
+                    }
+                }
+            },
+
+            cpal::UnknownTypeBuffer::F32(mut buffer) => {
+                for sample in buffer.chunks_mut(format.channels.len()) {
+                    // let value = next_value();
+                    for out in sample.iter_mut() {
+                        *out = value;
+                    }
+                }
+            },
+        };
+    });
+
+    // End Playback
 
     // A voice in cpal is used for playback.
-    let mut voice = cpal::Voice::new(&endpoint, &format).unwrap();
+    // let mut voice = cpal::Voice::new(&endpoint, &format).unwrap();
 
-    let mut samples_left = reader.len() as usize;
+    // event_loop.play(voice_id);
 
-    let mut append_data = |voice: &mut cpal::Voice| {
-        match voice.append_data(samples_left) {
-            cpal::UnknownTypeBuffer::I16(mut wrapped_buf) => {
-                // We cannot rely on Rust's autoderef here, because we want to
-                // call .len() on the buffer, which would cause a deref() of the
-                // buffer, not a deref_mut(), and cpal's deref() implementation
-                // is to panic.
-                let buf: &mut [i16] = &mut *wrapped_buf;
-                for (dst, src) in buf.iter_mut().zip(reader.samples::<i16>()) {
-                    *dst = src.unwrap();
-                }
-                samples_left -= buf.len();
-            }
-            cpal::UnknownTypeBuffer::F32(mut wrapped_buf) => {
-                let buf: &mut [f32] = &mut *wrapped_buf;
-                for (dst, src) in buf.iter_mut().zip(reader.samples::<f32>()) {
-                    *dst = src.unwrap();
-                }
-                samples_left -= buf.len();
-            }
-            _ => unreachable!()
-        }
+    // let mut samples_left = reader.len() as usize;
 
-        // Loop again if there are samples left.
-        samples_left > 0
-    };
+    // let mut append_data = |voice_id: &mut cpal::VoiceId| {
+    //     match voice_id.append_data(samples_left) {
+    //         cpal::UnknownTypeBuffer::I16(mut wrapped_buf) => {
+    //             // We cannot rely on Rust's autoderef here, because we want to
+    //             // call .len() on the buffer, which would cause a deref() of the
+    //             // buffer, not a deref_mut(), and cpal's deref() implementation
+    //             // is to panic.
+    //             let buf: &mut [i16] = &mut *wrapped_buf;
+    //             for (dst, src) in buf.iter_mut().zip(reader.samples::<i16>()) {
+    //                 *dst = src.unwrap();
+    //             }
+    //             samples_left -= buf.len();
+    //         }
+    //         cpal::UnknownTypeBuffer::F32(mut wrapped_buf) => {
+    //             let buf: &mut [f32] = &mut *wrapped_buf;
+    //             for (dst, src) in buf.iter_mut().zip(reader.samples::<f32>()) {
+    //                 *dst = src.unwrap();
+    //             }
+    //             samples_left -= buf.len();
+    //         }
+    //         _ => unreachable!()
+    //     }
 
-    // The voice must have some data before playing for the first time.
-    let mut has_more = append_data(&mut voice);
-    voice.play();
+    //     // Loop again if there are samples left.
+    //     samples_left > 0
+    // };
 
-    // Then we keep providing new data until the end of the audio.
-    while has_more {
-        has_more = append_data(&mut voice);
-    }
+    // // The voice must have some data before playing for the first time.
+    // let mut has_more = append_data(&mut voice_id);
+    // voice_id.play();
 
-    // Wait for playback to complete.
-    while voice.underflowed() {
-        thread::yield_now();
-    }
+    // // Then we keep providing new data until the end of the audio.
+    // while has_more {
+    //     has_more = append_data(&mut voice_id);
+    // }
+
+    // // Wait for playback to complete.
+    // while voice_id.underflowed() {
+    //     thread::yield_now();
+    // }
 }
 
-fn matches_format(format: &cpal::Format, spec: &hound::WavSpec) -> bool {
-    let cpal::SamplesRate(sample_rate) = format.samples_rate;
-    if sample_rate != spec.sample_rate {
-        return false
-    }
+// fn matches_format(format: &cpal::Format, spec: &hound::WavSpec) -> bool {
+//     let cpal::SamplesRate(sample_rate) = format.samples_rate;
+//     if sample_rate != spec.sample_rate {
+//         return false
+//     }
 
-    if format.channels.len() != spec.channels as usize {
-        return false
-    }
+//     if format.channels.len() != spec.channels as usize {
+//         return false
+//     }
 
-    let data_type = match (spec.bits_per_sample, spec.sample_format) {
-        (16, hound::SampleFormat::Int) => Some(cpal::SampleFormat::I16),
-        (32, hound::SampleFormat::Float) => Some(cpal::SampleFormat::F32),
-        _ => None
-    };
+//     let data_type = match (spec.bits_per_sample, spec.sample_format) {
+//         (16, hound::SampleFormat::Int) => Some(cpal::SampleFormat::I16),
+//         (32, hound::SampleFormat::Float) => Some(cpal::SampleFormat::F32),
+//         _ => None
+//     };
 
-    if Some(format.data_type) != data_type {
-        return false
-    }
+//     if Some(format.data_type) != data_type {
+//         return false
+//     }
 
-    // If the sample rate, channel count, and sample format match, then we can
-    // play back the file in this format.
-    true
-}
+//     // If the sample rate, channel count, and sample format match, then we can
+//     // play back the file in this format.
+//     true
+// }
